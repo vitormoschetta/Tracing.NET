78c2029a6a3f836a64443c4c87d480f74df0d2f9
Vitor <vfarias@bionexo.com>
2022-12-13 21:45:17 -0400

    add

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sample.DistributedTracing
+{
+    class Program
+    {
+        private static ActivitySource source = new ActivitySource(nameof(Sample.DistributedTracing));
+
+        static async Task Main(string[] args)
+        {
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MyService"))
+                .AddSource(nameof(Sample.DistributedTracing))
+                .AddConsoleExporter()
+                .Build();
+
+            await Execute();            
+        }
+        
+        static async Task Execute()
+        {
+            using (var activity = source.StartActivity(nameof(Execute)))
+            {
+                await StepOne();                
+                await StepTwo();                
+            }
+        }
+
+        static async Task StepOne()
+        {
+            using (var activity = source.StartActivity("StepOne"))
+            {
+                activity?.SetTag("tag1", "value1");
+                activity?.SetTag("tag2", "value2");
+                await Task.Delay(500);
+            }
+        }
+
+        static async Task StepTwo()
+        {
+            using (var activity = source.StartActivity("StepTwo"))
+            {
+                activity?.AddEvent(new ActivityEvent("StepTwo Event"));
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Tracing Distributed Systems with OpenTelemetry
+
+https://learn.microsoft.com/pt-br/dotnet/core/diagnostics/distributed-tracing-instrumentation-walkthroughs
+
+## Zipkin
+
+https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Zipkin/README.md
+
+https://zipkin.io/
+
+
+
+## Distributed Tracing Sample
+
+https://www.mytechramblings.com/posts/getting-started-with-opentelemetry-and-dotnet-core/
